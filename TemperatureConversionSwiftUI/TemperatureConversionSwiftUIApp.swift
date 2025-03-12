367b234c34e86a1b56c802878bc20c61aa923f88
Saputra <saputra.uta50@gmail.com>
2025-03-12 16:36:14 +0800

    day 19 first commit

---
+++ TemperatureConversionSwiftUI/TemperatureConversionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionSwiftUIApp.swift
+//  TemperatureConversionSwiftUI
+//
+//  Created by Saputra on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
